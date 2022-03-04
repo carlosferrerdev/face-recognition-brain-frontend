c3961c73e4b9f5f43aa86fc0777c8c4b700ee118
Carlos Ferrer <carlosvferrer@gmail.com>
2022-03-04 18:59:51 -0300

    Updates

---
+++ precache-manifest.4803c1aae32334ec0f7532f5703fbb45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f1ce1cdf5749788c29",
+    "url": "/face-recognition-brain-frontend/static/css/main.319260a2.chunk.css"
+  },
+  {
+    "revision": "e9f1ce1cdf5749788c29",
+    "url": "/face-recognition-brain-frontend/static/js/main.e9f1ce1c.chunk.js"
+  },
+  {
+    "revision": "462fada37566201ee509",
+    "url": "/face-recognition-brain-frontend/static/js/1.462fada3.chunk.js"
+  },
+  {
+    "revision": "e2c6ab61cac1c0c5f340",
+    "url": "/face-recognition-brain-frontend/static/css/2.effd584d.chunk.css"
+  },
+  {
+    "revision": "e2c6ab61cac1c0c5f340",
+    "url": "/face-recognition-brain-frontend/static/js/2.e2c6ab61.chunk.js"
+  },
+  {
+    "revision": "e285f5635aacd3a62a4e",
+    "url": "/face-recognition-brain-frontend/static/js/runtime~main.e285f563.js"
+  },
+  {
+    "revision": "28f7521dd5958b10f970ea5a5e42caa0",
+    "url": "/face-recognition-brain-frontend/index.html"
+  }
+];
